229a473bb9080f98183f1188831b0a4398a1c60f
Welliton Paiva <wellitonpaiva@gmail.com>
2022-08-06 22:21:01 +0100

    adding first ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: '', 
+      result: '',
+      duplications: [],
+      uniqueElements: []
+    }
+
+    this.handleText = this.handleText.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleText(e) {
+    this.setState({text: e.target.value})
+
+  }
+
+  handleClick() {
+    let resultHtml = "";
+    const duplications = this.findDuplication(this.state.text)  
+    this.state.text.split(' ').map(word => {
+      if(duplications.find(w => w === word)) {
+        resultHtml += "<p class='duplicated result'>" + word +" &nbsp;</p>"
+      } else {
+        resultHtml += "<p class='result'>" + word +"&nbsp;</p>"
+      }
+    })
+    this.setState({result: resultHtml})
+  }
+
+  findDuplication(str) {
+    const words = str.split(' ')
+    const uniqueElements = new Set(words);
+    const filteredElements = words.filter(item => {
+        if (uniqueElements.has(item)) {
+            uniqueElements.delete(item);
+        } else {
+            return item;
+        }
+    });
+
+    return filteredElements
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+            <h1>Calliope</h1>
+            <h2>Revisor de textos</h2>
+          </header>
+          <main className="App-main">
+            <div>
+              <TextField id="text" 
+                className="textfield" 
+                label="Insira seu texto aqui" 
+                placeholder="Insira seu texto aqui" 
+                value={this.state.text}
+                onChange={this.handleText}
+                multiline/>
+              <Button variant="outlined" onClick={this.handleClick}>Revise</Button>
+            </div>
+            <div>
+              <p className='result' dangerouslySetInnerHTML={{__html: this.state.result}}></p>
+            </div>
+          </main>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
